bd7b55b89cb5ebe03aabe35f6d1485cb25aaade3
Abhinav singh <abhinavsinh014@gmail.com>
2026-01-27 12:35:06 +0530

    added question file

--- list3.py
+++ list3.py
@@ -1,7 +1,10 @@
+#printing list elements
 list = [1,2,3]
 x = len(list)
 for i in range(0,x):
     print(list[i],end = " ")
 
+print(" ")
+ #adding list
 list1 = list + [4]
 print(list1)
---
+++ listquestions.py
@@ -0,0 +1,9 @@
+#To all the elements of list
+
+spydi = int(input())
+stark = input().split()
+starksum = 0
+for i in range(spydi):
+    starksum += int(stark[i])
+
+print(starksum)
