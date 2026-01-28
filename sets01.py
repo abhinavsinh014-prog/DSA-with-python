fffc58b4a6da0e8efff24776b10493e6f6531d97
Abhinav singh <abhinavsinh014@gmail.com>
2026-01-28 23:55:32 +0530

    added a set file

---
+++ sets01.py
@@ -0,0 +1,13 @@
+sets = {1,5,9,2,2,4,6,2,6}
+print(sets)
+sets.add(3)
+print(sets)
+take = {7,8}
+sets.update(take)
+print(sets)
+sets.remove(1)
+print(sets)
+sets.discard(2)
+print(sets)
+sets.clear
+print(sets)
