cc99b0cc8e7f59a78eb6ce827fe310dc278c07c3
Abhinav singh <abhinavsinh014@gmail.com>
2026-01-28 23:37:45 +0530

    make a result data by using dictionary

---
+++ dictionary.py
@@ -0,0 +1,9 @@
+dick = { 
+    "aditi" : 954,
+    "naman" : 898,
+    "randy" : 656,
+    "john"  : 918
+}
+print(dick.keys())
+print(dick.values())
+print(dick.items())
---
+++ dictionary01.py
@@ -0,0 +1,24 @@
+dick = { 
+    "aditi" : 954,
+    "naman" : 898,
+    "randy" : 656,
+    "john"  : 318,
+    "pandat" : 312,
+    "gadariya":512,
+    "mohit"  : 289
+}
+
+# for key,value in dick.items():
+#     print(f"{key} has scored {value} out of 1000")
+
+search = input()
+if search in dick:
+    marks = dick[search]
+    percantage = marks/10
+    if marks < 333:
+        print(" FAIL",)
+    else:
+        print(" PASS")
+    print(f"Name:-{search} Marks:- {dick[search]} percantage:- {percantage}")
+else:
+    print("student not found")
