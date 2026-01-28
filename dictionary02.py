84a5e6b4e8466257a2c7b9a120891e8e6353cf0b
Abhinav singh <abhinavsinh014@gmail.com>
2026-01-28 23:47:45 +0530

    added  a new file

---
+++ dictionary02.py
@@ -0,0 +1,12 @@
+dick = { 
+    "aditi" : 954,
+    "naman" : 898,
+    "randy" : 656,
+    "john"  : 318,
+    "pandat" : 312,
+    "gadariya":512,
+    "mohit"  : 289
+}
+
+for key,value in dick.items():
+    print(f"{key} has scored {value} out of 1000")
