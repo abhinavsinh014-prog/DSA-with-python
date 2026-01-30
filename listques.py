25a186508a5e9d7a68903f228ed8038bdb39ed26
Abhinav singh <abhinavsinh014@gmail.com>
2026-01-30 23:48:35 +0530

    added

--- listques.py
+++ listques.py
@@ -18,3 +18,12 @@ student = {
 
 for key, value in student.items():
     print(key, ":", value)
+
+marks = {
+    "Math": 85,
+    "Science": 90,
+    "English": 88
+}
+
+if "Math" in marks:
+    print("Math marks:", marks["Math"])
