c8b519b53dbb0b46c4b6705d15cfca9ab1b823f8
Abhinav singh <abhinavsinh014@gmail.com>
2026-01-31 22:18:32 +0530

    added a question file

--- practice.py
+++ practice.py
@@ -1,4 +1,8 @@
 #write all the even number from 0 to 100 in one line 
 
-for i in range(0,101,2):
-    print(i,end = " ")
+# for i in range(0,101,2):
+#     print(i,end = " ")
+
+#write all number divide by 7 till 120
+for i in range(7,120,7):
+    print(i) 
