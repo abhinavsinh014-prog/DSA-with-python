2fab44c4c86a761804db5d80db3962e66c796793
Abhinav singh <abhinavsinh014@gmail.com>
2026-02-01 20:02:29 +0530

    created calculator

---
+++ calculator.py
@@ -0,0 +1,24 @@
+while True:
+    operation = input("Enter operation (+, -, *, /): ")
+    if operation == 'exit':
+        print("Exiting the calculator. Goodbye!")
+        break
+    num1 = float(input("Enter first number: "))
+    num2 = float(input("Enter second number: "))
+    
+    if operation == '+':
+        result = num1 + num2
+    elif operation == '-':
+        result = num1 - num2 
+    elif operation == '*':
+        result = num1 * num2
+    elif operation == '/':
+        if num2 != 0:
+            result = num1 / num2
+        else:
+            result = "Error! Division by zero."
+    else:
+        result = "Invalid operation."
+    print("result",result) 
+       
+    
