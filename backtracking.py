bec1d818c1c77bc0780272ca451367deaac39ede
Abhinav singh <abhinavsinh014@gmail.com>
2026-02-03 23:30:03 +0530

    backtracking

---
+++ backtracking.py
@@ -0,0 +1,23 @@
+class Solution:
+    def permute(self, nums):
+        result = []
+
+        def backtrack(path, remaining):
+            if not remaining:
+                result.append(path)
+                return
+
+            for i in range(len(remaining)):
+                backtrack(
+                    path + [remaining[i]],
+                    remaining[:i] + remaining[i+1:]
+                )
+
+        backtrack([], nums)
+        return result
+
+
+# Testing
+nums = [1, 2, 3]
+sol = Solution()
+print(sol.permute(nums))
