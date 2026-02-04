aa74095493d644a2aad41b74db8ae0fd4955e203
Abhinav singh <abhinavsinh014@gmail.com>
2026-02-04 22:24:33 +0530

    added a new file

--- dataoutliers.py
+++ dataoutliers.py
@@ -1,10 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-
 dataset = [11,12,13,15,16,14,18,17,780,985,17,15,16,14,13,12,18,754,19,11,789,17,17,16]
 
-
+plt.hist(dataset, bins=10, edgecolor='black')
+plt.title("Histogram to show outliers")
 print("Mean:", np.mean(dataset))
 print("Median:", np.median(dataset)) 
-
+plt.show()
