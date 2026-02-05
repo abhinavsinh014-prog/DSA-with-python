05a99032482d1cb2521a231c4093808125641c2a
Abhinav singh <abhinavsinh014@gmail.com>
2026-02-05 23:20:52 +0530

    practising some codes

---
+++ practicecodes.py
@@ -0,0 +1,13 @@
+numbers = [12, 45, 7, 89, 23]
+
+largest = numbers[0]
+smallest = numbers[0]
+
+for num in numbers:
+    if num > largest:
+        largest = num
+    if num < smallest:
+        smallest = num
+
+print("Largest:", largest)
+print("Smallest:", smallest)
