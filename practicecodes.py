fda386391eef6b38b54f3bf8271dd7350db60a8a
Abhinav singh <abhinavsinh014@gmail.com>
2026-02-05 23:22:43 +0530

    2nd practise code

--- practicecodes.py
+++ practicecodes.py
@@ -1,3 +1,4 @@
+# This code finds the largest and smallest numbers in a list of numbers.
 numbers = [12, 45, 7, 89, 23]
 
 largest = numbers[0]
@@ -11,3 +12,18 @@ for num in numbers:
 
 print("Largest:", largest)
 print("Smallest:", smallest)
+
+# This code counts the number of even and odd numbers in a list of numbers.
+nums = [1, 2, 3, 4, 5, 6, 7]
+
+even = 0
+odd = 0
+
+for n in nums:
+    if n % 2 == 0:
+        even += 1
+    else:
+        odd += 1
+
+print("Even:", even)
+print("Odd:", odd)
