2ef436b846b77938a7ad769787e34478122d9dfa
Abhinav singh <abhinavsinh014@gmail.com>
2026-02-07 20:30:16 +0530

    added new lines of code

--- ATMsumulation.py
+++ ATMsumulation.py
@@ -1,14 +1,14 @@
 bank = { 
-    "aditi" : {"balance":95454, "pin":1234},
-    "naman" : {"balance":89878, "pin":5678},
-    "randy" : {"balance":65256, "pin":9012},
-    "john"  : {"balance":31218, "pin":3456},
-    "pandat" : {"balance":31982, "pin":7890},
-    "gadariya":{"balance":56512, "pin":2345},
-    "mohit"  : {"balance":28879, "pin":4567}
+    "aditi" : {"balance":95454, "PIN":1234},
+    "naman" : {"balance":89878, "PIN":5678},
+    "randy" : {"balance":65256, "PIN":9012},
+    "john"  : {"balance":31218, "PIN":3456},
+    "pandat" : {"balance":31982, "PIN":7890},
+    "gadariya":{"balance":56512, "PIN":2345},
+    "mohit"  : {"balance":28879, "PIN":4567}
 }
 running = True
-print("======= ATM =======")
+print("======= CHINGI BANK ATM =======")
 
 while running :
     todo = input("Want to open new account or already have account (old/new/exit):-").lower().strip()
