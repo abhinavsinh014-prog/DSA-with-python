c7b04029fc06dfbf244b9cc76b9aff0019ae0d6d
Abhinav singh <abhinavsinh014@gmail.com>
2026-02-10 18:32:06 +0530

    using module

--- array.py
+++ array.py
@@ -8,3 +8,25 @@ for x in a1:
 
 for i in range(4):
     print(a1[i],end=",")
+print(" ")
+c=0
+while (c<len(a1)):
+    print(a1[c])
+    c+=1
+
+a1.append(31)
+print(a1)
+a1.count(31)
+print(a1)
+a1.extend([69])
+print(a1)
+# a1.fromlist(65)
+a1.index(31)
+print(a1)
+a1.pop(1)
+print(a1)
+a1.reverse()
+print(a1)
+# a1.tolist()
+a1.remove(24)
+print(a1)
