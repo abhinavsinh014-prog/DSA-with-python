8e34f4dd3bf67b83ce9c3ebb088d04fb9a2abe10
Abhinav singh <abhinavsinh014@gmail.com>
2026-02-11 19:37:00 +0530

    two question solved

--- q.py
+++ q.py
@@ -1,19 +1,31 @@
-arr = [1,2,3,6,5,4,9,8,7]
-target = 15
-curr_sum = 0
-left=0
-found = False
-for right in range(len(arr)):
-            curr_sum += arr[right]
+# arr = [1,2,3,6,5,4,9,8,7]
+# target = 9
+# curr_sum = 0
+# left=0
+# found = False
+# for right in range(len(arr)):
+#             curr_sum += arr[right]
 
-            # shrink window if sum exceeds target
-            while curr_sum > target and left <= right:
-                curr_sum -= arr[left]
-                left += 1
+#             # shrink window if sum exceeds target
+#             while curr_sum > target and left <= right:
+#                 curr_sum -= arr[left]
+#                 left += 1
 
-            if curr_sum == target:
-                print([left + 1, right + 1])
-                found = True
-                break
-if not found:
-       print([-1])
+#             if curr_sum == target:
+#                 print([left + 1, right + 1])
+#                 found = True
+#                 break
+# if not found:
+#        print([-1])
+
+arr = [2,6,4,8,1,3,5,9]
+m = len(arr)+1
+
+total_sum = m*(m+1)//2
+
+arr_sum = 0
+for i in arr:
+    arr_sum += i
+
+missing = total_sum - arr_sum
+print(missing)
