b2f89d72c1f5c1543013a14a0c23b45e9b0ca49c
Abhinav singh <abhinavsinh014@gmail.com>
2026-02-13 23:44:27 +0530

    added new file

--- DSAquestionsolved.py
+++ DSAquestionsolved.py
@@ -164,19 +164,52 @@
 
 class Solution:
     def sort012(self, arr):
-        low = 0
-        mid = 0
-        high = len(arr) - 1
+        c0 = c1 = c2 = 0
         
-        while mid <= high:
-            if arr[mid] == 0:
-                arr[low], arr[mid] = arr[mid], arr[low]
-                low += 1
-                mid += 1
+        for num in arr:
+            if num == 0:
+                c0 += 1
+            elif num == 1:
+                c1 += 1
+            else:
+                c2 += 1
+        
+        i = 0
+        
+        while c0 > 0:
+            arr[i] = 0
+            i += 1
+            c0 -= 1
+            
+        while c1 > 0:
+            arr[i] = 1
+            i += 1
+            c1 -= 1
+            
+        while c2 > 0:
+            arr[i] = 2
+            i += 1
+            c2 -= 1
+ans = Solution()
+print(ans.sort012([0,2,0,1,2,1,0,2]))
+
+# class Solution():
+#     def sort012(self, arr):
+#         low = 0
+#         mid = 0
+#         high = len(arr) - 1
+        
+#         while mid <= high:
+#             if arr[mid] == 0:
+#                 arr[low], arr[mid] = arr[mid], arr[low]
+#                 low += 1
+#                 mid += 1
                 
-            elif arr[mid] == 1:
-                mid += 1
+#             elif arr[mid] == 1:
+#                 mid += 1
                 
-            else:  # arr[mid] == 2
-                arr[mid], arr[high] = arr[high], arr[mid]
-                high -= 1
+#             else:  # arr[mid] == 2
+#                 arr[mid], arr[high] = arr[high], arr[mid]
+#                 high -= 1
+# ans = Solution()
+# print(ans.sort012([0,2,0,1,2,1,0,2]))
