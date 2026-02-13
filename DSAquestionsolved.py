1f9967e8a256c2650ab5df7d2827c51b04a59c1b
Abhinav singh <abhinavsinh014@gmail.com>
2026-02-13 22:54:36 +0530

    added new file

--- DSAquestionsolved.py
+++ DSAquestionsolved.py
@@ -128,3 +128,19 @@ for i in range(1,len(arr)):
     for j in range(i,len(arr)):
         if arr[i]<arr[j]:
             print(arr[j],end=' ')
+
+class Solution:
+    def leaders(self, arr):
+        n = len(arr)
+        result = []
+        
+        max_from_right = arr[-1]
+        result.append(max_from_right)
+        
+        for i in range(n-2, -1, -1):
+            if arr[i] >= max_from_right:
+                max_from_right = arr[i]
+                result.append(arr[i])
+        
+        result.reverse()
+        return result
